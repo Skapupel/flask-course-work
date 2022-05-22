7450e73c9999b70949bdf084713020835cda7e5f
Viacheslav <bkd.slava@gmail.com>
2022-05-22 15:07:45 +0300

    Logic changed

--- flasksite/views/bot/controllers/bot_commands.py
+++ flasksite/views/bot/controllers/bot_commands.py
@@ -7,12 +7,13 @@ from flasksite.views.bot.controllers.messages_text import TEXT
 
 
 def start(chat_id, username):
-    if username and not Users.query.filter_by(username=username).first():
-        password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(8)))
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        user = Users(username, hashed_password, chat_id)
-        db.session.add(user)
-        db.session.commit()
+    if username:
+        if not Users.query.filter_by(username=username).first():
+            password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(8)))
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            user = Users(username, hashed_password, chat_id)
+            db.session.add(user)
+            db.session.commit()
         sendMessage(chat_id, TEXT['start_with_username'])
     else:
         sendMessage(chat_id, TEXT['start_without_username'])
